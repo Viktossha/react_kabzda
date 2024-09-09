112392b3637fff9a074e08fabbacc4604cc4cdb0
Vika <vikapetrova8@gmail.com>
2024-09-09 19:40:41 +0300

    02 components

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,61 @@ import './App.css';
 
 //function declaration
 function App() {
+    console.log('App rendering')
     return (
         <div>
-            hello
-          <Rating/>
-          <Accordion/>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
 
+function AppTitle() {
+    console.log('AppTitle rendered')
+    return <>This is app component</>
+}
+
 function Rating() {
+    console.log('Rating rendering')
     return (
         <div>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
     )
 }
 
 function Star() {
-  return <div>star</div>
+    console.log('Star rendering')
+    return <div>star</div>
 }
 
 function Accordion() {
-  return (
-      <div>
-        <h3>Меню</h3>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      </div>
-  )
+    console.log('Accordion rendered')
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendered')
+    return <h3>Меню</h3>
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendered')
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 }
 
 export default App;
